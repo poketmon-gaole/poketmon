83191830337d98da36e17ecdc1ae7f6e783fe9cd
juwan <park.ju.wan@gmail.com>
2022-08-17 10:53:36 +0900

    first commit

---
+++ src/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="포맷몬 가오레 로고" src="./assets/poketmon.png" height="170">
+  <HelloWorld msg="박시우 환영해~ ^^"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
