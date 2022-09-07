41301360aa77b6303d5ce995de51d9bc977b0850
박주완 <park.ju.wan@gmail.com>
2022-09-07 13:26:47 +0900

    no message

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000
 const distDir = __dirname + "/dist/";
 
 // SERVES FILES from our dist directory which now contains out index.html
-app.use(express.static(distDir));
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use(express.static(distDir));
 app.listen(port)
 console.log('Listening on port:' + port)
 // app.get('/', (req, res) => res.send('Hello World!'))
