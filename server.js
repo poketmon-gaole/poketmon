7b1bb399a2391c14d7d56f5796faedd614209caf
박주완 <park.ju.wan@gmail.com>
2022-09-09 15:54:33 +0900

    server.js 테스트

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000
 // SERVES FILES from our dist directory which now contains out index.html
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
 app.listen(port)
+
+if(process.env.NODE.ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+  });  
+}
 // console.log('Listening on port:' + port)
 // app.get('/', (req, res) => res.send('Hello World!'))
 // app.listen(port, () => console.log(`Example app listening on port port!`))
