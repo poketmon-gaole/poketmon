6e4ac9d68d6831757afed61dd3e43120f853160c
박주완 <park.ju.wan@gmail.com>
2022-09-09 15:59:42 +0900

    server.js 테스트

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-// SERVES FILES from our dist directory which now contains out index.html
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
-app.listen(port)
-
 if(process.env.NODE.ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'front_end', 'build')));
   
