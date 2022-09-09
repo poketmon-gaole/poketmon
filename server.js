5fdcc4d199a9b959fd26e080e9d072b7c42e7e5a
박주완 <park.ju.wan@gmail.com>
2022-09-09 16:01:39 +0900

    server.js 테스트

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-if(process.env.NODE.ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
-  });  
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 // console.log('Listening on port:' + port)
 // app.get('/', (req, res) => res.send('Hello World!'))
