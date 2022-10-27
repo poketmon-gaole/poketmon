5c4321f199cec6aa948f762f8cc84cee82cf2c26
박주완 <park.ju.wan@gmail.com>
2022-10-27 16:54:42 +0900

    카카오 애드핏 주석

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,15 +1,19 @@
 <template>
+  <!--
   <ins class="kakao_ad_area" style="display:none;"
   data-ad-unit    = "DAN-e5cqaGXR9fpc4zco"
   data-ad-width   = "320"
   data-ad-height  = "50"></ins>
+  -->
   <div class="contents">
     <h1>{{ setTitle() }}</h1>
     <template v-for="grade in gradeList" :key="grade">
+      <!--
       <ins v-if="gradeList.length > 1 && grade == 4" class="kakao_ad_area" style="display:none;"
         data-ad-unit    = "DAN-p7nNGyfhJYBPvnQl"
         data-ad-width   = "320"
-        data-ad-height  = "100"></ins>      
+        data-ad-height  = "100"></ins>
+      -->
       <h3>GRADE {{ grade }}</h3>
       <ul>
         <template v-for="(item, index) in data" :key="index">
